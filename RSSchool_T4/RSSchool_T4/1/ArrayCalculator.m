6d814dad99b572cbbfa449677f625c7975442c82
Volha Bychok <olgabychok88@gmail.com>
2020-04-12 22:02:38 +0300

    2 tasks

--- RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
+++ RSSchool_T4/RSSchool_T4/1/ArrayCalculator.m
@@ -2,7 +2,70 @@
 
 @implementation ArrayCalculator
 + (NSInteger)maxProductOf:(NSInteger)numberOfItems itemsFromArray:(NSArray *)array {
-    // good luck
-    return -1;
+    NSMutableArray *newArray = [NSMutableArray new];
+    for (int i = 0; i < array.count; i++) {
+        if([[array objectAtIndex:i] isKindOfClass:[NSNumber class]])
+        {
+            [newArray addObject:[array objectAtIndex:i]];
+        }
+    }
+    NSSortDescriptor *highestToLowest = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO];
+    NSArray *newArray2 = [newArray sortedArrayUsingFunction:intSort context:NULL];
+    [newArray sortUsingDescriptors:[NSArray arrayWithObject:highestToLowest]];
+    if (numberOfItems == 0) {
+        return 0;
+    } else if (numberOfItems > newArray.count) {
+        numberOfItems = newArray.count;
+    }
+    NSNumber *n = [NSNumber new];
+    n = [newArray firstObject];
+    for (int i = 1; i < numberOfItems; i++) {
+        n =[NSNumber numberWithInteger: [n intValue] * [[newArray objectAtIndex:i] intValue]];
+    }
+    NSNumber *n2 = [NSNumber new];
+    n2 = [newArray2 firstObject];
+    for (int i = 1; i < numberOfItems; i++) {
+        int a = [n2 intValue];
+        int b = [[newArray2 objectAtIndex:i] intValue];
+        int c = a * b;
+        n2 =[NSNumber numberWithInteger: c];
+    }
+    if ([n2 intValue] < [n intValue]) {
+        return [n intValue];
+    } else {
+        return [n2 intValue];
+    }
 }
+
++(NSArray *)sort:(NSArray*)array {
+    NSMutableArray *newArray = [NSMutableArray new];
+    for (int i = 0; i < array.count; i++) {
+        if ([[array objectAtIndex:i] intValue] < 0) {
+            if ([[array objectAtIndex:i] intValue] * -1 > [newArray.firstObject intValue]) {
+                [newArray insertObject:[array objectAtIndex:i] atIndex:0];
+            }
+        }
+    }
+    return array;
+}
+
+NSInteger intSort(id num1, id num2, void *context)
+{
+    int v1 = [num1 intValue];
+    int v2 = [num2 intValue];
+    if (v1 < 0) {
+        v1 = v1 * -1;
+    }
+    if (v2 < 0) {
+        v2 = v2 * -1;
+    }
+    if (v1 > v2)
+        return NSOrderedAscending;
+    else if (v1 < v2)
+        return NSOrderedDescending;
+    else
+        return NSOrderedSame;
+}
+
+
 @end
--- RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
+++ RSSchool_T4/RSSchool_T4/3/TelephoneFinder.m
@@ -2,7 +2,112 @@
 
 @implementation TelephoneFinder
 - (NSArray <NSString*>*)findAllNumbersFromGivenNumber:(NSString*)number {
-    // good luck
-    return @[@"0"];
+    if ([number intValue] < 0 ) {
+        return nil;
+    }
+    NSMutableArray *result = [NSMutableArray new];
+    NSArray *array1 = @[@2, @4];
+    NSArray *array2 = @[@1, @5, @3];
+    NSArray *array3 = @[@6, @2];
+    NSArray *array4 = @[@1, @5, @7];
+    NSArray *array5 = @[@2, @6, @4, @8];
+    NSArray *array6 = @[@3, @5, @9];
+    NSArray *array7 = @[@4, @8];
+    NSArray *array8 = @[@9, @5, @7, @0];
+    NSArray *array9 = @[@8, @6];
+    NSArray *array0 = @[@8];
+    NSMutableArray *array = [[NSMutableArray alloc] init];
+    for (int i = 0; i < number.length; i ++) {
+        [array addObject:[NSString stringWithFormat:@"%c", [number characterAtIndex:i]]];
+    }
+    for (int i = 0; i < array.count; i ++) {
+        NSString *num = array[i];
+        switch ([num intValue]) {
+          case 0:
+            for (int a = 0; a < array0.count; a ++) {
+                array[i] = [NSString stringWithString:array0[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 1:
+            for (int a = 0; a < array1.count; a ++) {
+                array[i] = [NSString stringWithString:array1[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 2:
+            for (int a = 0; a < array2.count; a ++) {
+                array[i] = [NSString stringWithString:array2[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 3:
+            for (int a = 0; a < array3.count; a ++) {
+                array[i] = [NSString stringWithString:array3[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 4:
+            for (int a = 0; a < array4.count; a ++) {
+                array[i] = [NSString stringWithString:array4[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 5:
+            for (int a = 0; a < array5.count; a ++) {
+                array[i] = [NSString stringWithString:array5[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 6:
+            for (int a = 0; a < array6.count; a ++) {
+                array[i] = [NSString stringWithString:array6[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 7:
+            for (int a = 0; a < array7.count; a ++) {
+                array[i] = [NSString stringWithString:array7[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 8:
+            for (int a = 0; a < array8.count; a ++) {
+                array[i] = [NSString stringWithString:array8[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+            case 9:
+            for (int a = 0; a < array9.count; a ++) {
+                array[i] = [NSString stringWithString:array9[a]];
+                NSString *str = [array componentsJoinedByString:@""];
+                [result addObject:str];
+            }
+            array[i] = [NSString stringWithString:num];
+            break;
+          default:
+            break;
+        }
+    }
+    return result;
 }
+
 @end
